60783cce7d484c2130ff5b79afe691b4fa73250a
picksonamp <167912564+picksonamp@users.noreply.github.com>
2024-04-23 22:55:08 +0400

    Приглашение на Свадьбу

---
+++ script.js
@@ -0,0 +1,137 @@
+
+let i = 0;
+
+let end = new Date('2024-07-05T15:45:00.417')
+end = end.getTime()
+
+
+
+setInterval(() => {
+
+    let start = Date.now()
+    
+
+    let days = Math.trunc((end - start) / 1000 / 60 / 60 / 24)
+    let hours = Math.trunc((end - start) / 1000 / 60 / 60)
+    let minutes = Math.trunc((end - start) / 1000 / 60)
+    let seconds = Math.trunc((end - start) / 1000)
+
+    switch (days % 10) { 
+        case 1: document.getElementById('daysIndex').innerHTML = 'ДЕНЬ'; break;
+        case 2:
+        case 3:
+        case 4: document.getElementById('daysIndex').innerHTML = 'ДНЯ'; break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 0: document.getElementById('daysIndex').innerHTML = 'ДНЕЙ'; break;
+    }
+
+    switch (hours % 10) { 
+        case 1: document.getElementById('hoursIndex').innerHTML = 'ЧАС'; break;
+        case 2:
+        case 3:
+        case 4: document.getElementById('hoursIndex').innerHTML = 'ЧАСА'; break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 0: document.getElementById('hoursIndex').innerHTML = 'ЧАСОВ'; break;        
+    }
+
+    switch (minutes % 10) { 
+        case 1: document.getElementById('minutesIndex').innerHTML = 'МИНУТА'; break;
+        case 2:
+        case 3:
+        case 4: document.getElementById('minutesIndex').innerHTML = 'МИНУТЫ'; break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 0: document.getElementById('minutesIndex').innerHTML = 'МИНУТ'; break;        
+    }
+
+    switch (seconds % 10) { 
+        case 1: document.getElementById('secondsIndex').innerHTML = 'СЕКУНДА'; break;
+        case 2:
+        case 3:
+        case 4: document.getElementById('secondsIndex').innerHTML = 'СЕКУНДЫ'; break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 0: document.getElementById('secondsIndex').innerHTML = 'СЕКУНД'; break;        
+    }
+
+    
+
+   
+
+    document.getElementById('days').innerHTML = days
+    document.getElementById('hours').innerHTML = hours - (days * 24)
+    document.getElementById('minutes').innerHTML = minutes - (hours * 60)
+    document.getElementById('seconds').innerHTML = seconds - (minutes * 60)
+    
+}
+, 1000)
+
+// const vines = {
+//     redDry: '',
+//     redSemi: '',
+//     whiteDry: '',
+//     whiteSemi: '',
+//     vodka: '',
+//     shamp: '',
+//     alcFree: '',
+// }
+
+sendButton.onclick = () => {     
+
+    let name = document.getElementById('name').value
+
+    if (name === '') return alert('Введите имя!')
+
+    let presence = document.getElementById('presense').checked
+
+    if (!presence) {
+        localStorage.setItem(localStorage.length + 1, `${name} не придет`)
+        console.log(localStorage)
+        return alert('Данные отправлены!')  
+    }
+    else {
+    
+    let reultVines =`${name} придет! Будет пить: `
+
+    let redDry = document.getElementById('RedDry').checked
+    let redSemi = document.getElementById('RedSemi').checked
+    let whiteDry = document.getElementById('WhiteDry').checked
+    let whiteSemi = document.getElementById('WhiteSemi').checked
+    let shamp = document.getElementById('Shamp').checked
+    let alcFree = document.getElementById('AlcFree').checked
+
+    let custom = document.getElementById('textarea').value
+
+    reultVines += (redDry) ? "красное сухое вино | " : '' 
+    reultVines += (redSemi) ? "красное полусладкое вино | " : ''
+    reultVines += (whiteDry) ? "белое сухое вино | " : ''
+    reultVines += (whiteSemi) ? "белое полусладкое вино | " : ''
+    reultVines += (shamp) ? "шампанское | " : ''
+    reultVines += (alcFree) ? "безалкогольные напитки | " : ''
+    reultVines += (custom === null) ? '' : `${custom} |`
+
+    localStorage.setItem(++localStorage.length, reultVines)
+  
+    
+    console.log(localStorage)
+
+    alert('Данные отправлены!')
+
+    
+    }
+
+}
